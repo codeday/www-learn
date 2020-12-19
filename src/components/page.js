14f31f46c6306f772bfd8be3df7228edee7366ff
Bobcat <bobcat4848@gmail.com>
2020-12-19 17:36:39 -0600

    Getting format of website down

--- src/components/page.js
+++ src/components/page.js
@@ -1,7 +1,9 @@
 import { DefaultSeo } from 'next-seo';
 import Box from '@codeday/topo/Atom/Box';
-import Header, { SiteLogo } from '@codeday/topo/Organism/Header';
+import Header, { SiteLogo, Menu } from '@codeday/topo/Organism/Header';
+import Text, { Link } from '@codeday/topo/Atom/Text';
 import Footer from '@codeday/topo/Organism/Footer';
+import Button from '@codeday/topo/Atom/Button';
 import { CodeDay } from '@codeday/topo/Atom/Logo';
 
 // TODO: Set production domain
@@ -10,14 +12,14 @@ const DOMAIN = 'https://learn.codeday.org';
 export default ({ children, title, darkHeader, slug }) => (
   <>
     <DefaultSeo
-      title={title}
-      description="www-learn description"
-      canonical={`${DOMAIN}${slug}`}
+      title={`${title ? `${title} ~ ` : ''}CodeDay Learn`}
+      description="Continue your coding adventure"
+      canonical={`https://learn.codeday.org${slug}`}
       openGraph={{
         type: 'website',
         locale: 'en_US',
-        site_name: 'www-learn',
-        url: `${DOMAIN}${slug}`,
+        site_name: 'CodeDay Learn',
+        url: `https://learn.codeday.org${slug}`,
       }}
       twitter={{
         handle: '@codeday',
@@ -31,7 +33,26 @@ export default ({ children, title, darkHeader, slug }) => (
           <a href="/">
             <CodeDay withText />
           </a>
+          <a href="/">
+            <Text
+              as="span"
+              d="inline"
+              letterSpacing="-2px"
+              fontFamily="heading"
+              position="relative"
+              top={1}
+              ml={1}
+              textDecoration="underline"
+              bold
+            >
+              Learn
+            </Text>
+          </a>
         </SiteLogo>
+        <Menu>
+          <Button variant="ghost" variantColor="brand" as="a" href="/sponsor">Tracks</Button>
+          <Button variant="ghost" variantColor="brand" as="a" href="/volunteer">Contact</Button>
+        </Menu>
       </Header>
       {children}
       <Footer />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,72 @@
 
 import { Heading } from '@codeday/topo/Atom/Text';
 import Content from '@codeday/topo/Molecule/Content';
+import Divider from '@codeday/topo/Atom/Divider';
+import Text, { Link } from '@codeday/topo/Atom/Text';
+import Box from '@codeday/topo/Atom/Box';
+import Button from '@codeday/topo/Atom/Button';
 import Page from '../components/page';
+import IconBox, {HeaderIcon, HeaderText, Body} from '@codeday/topo/Molecule/IconBox';
+import Image from '@codeday/topo/Atom/Image';
 
 export default function Home() {
 	return (
 		<Page slug="/">
+			<Content textAlign="center">
+				<Heading as="h1">Learn At Undescribable Speeds</Heading>
+				<Text>CodeDay Learn is a new program aimed on helping newcomers and experienced programmers continue learning.</Text>
+			</Content>
+
+			<Divider/>
+
 			<Content>
-				<Heading as="h2" fontSize="5xl" textAlign="center">www-learn</Heading>
+				<Box textAlign="center">
+					<Heading as="h4">Browse the available tracks you can master</Heading>
+				</Box>
+
+			</Content>
+
+			<Divider/>
+
+			<Content>
+				<Text>Not sure which track to start on? Take a quiz to find your interests!</Text>
+				<Box w="100%"><Button>Take Quiz</Button></Box>
+
+				<IconBox maxWidth={300} d="inline-block">
+					<HeaderIcon><Image src="https://placekitten.com/64/64" borderRadius="full" /></HeaderIcon>
+					<HeaderText>Construct</HeaderText>
+					<Body>Start here if you're a complete begineer and finish by making your own game!</Body>
+    		</IconBox>
+
+				<IconBox maxWidth={300} d="inline-block">
+					<HeaderIcon><Image src="https://placekitten.com/64/64" borderRadius="full"/></HeaderIcon>
+					<HeaderText>Unity</HeaderText>
+					<Body>Take yourself to the next step and learn the fundamentals of using the Unity Engine</Body>
+    		</IconBox>
+
+				<IconBox maxWidth={300} d="inline-block">
+					<HeaderIcon><Image src="https://placekitten.com/64/64" borderRadius="full"/></HeaderIcon>
+					<HeaderText>Unreal</HeaderText>
+					<Body>Take yourself to the next step and learn the fundamentals of using the Unreal Engine</Body>
+    		</IconBox>
+
+				<IconBox maxWidth={300} d="inline-block">
+					<HeaderIcon><Image src="https://placekitten.com/64/64" borderRadius="full"/></HeaderIcon>
+					<HeaderText>Python</HeaderText>
+					<Body>Learn to program with Python, which is a simple and powerful language for begineers</Body>
+    		</IconBox>
+
+				<IconBox maxWidth={300} d="inline-block">
+					<HeaderIcon><Image src="https://placekitten.com/64/64" borderRadius="full"/></HeaderIcon>
+					<HeaderText>Java</HeaderText>
+					<Body>Learn to program with Java, which has been tried and tested to be one of the best languages out there!</Body>
+    		</IconBox>
+
+				<IconBox maxWidth={300} d="inline-block">
+					<HeaderIcon><Image src="https://placekitten.com/64/64" borderRadius="full"/></HeaderIcon>
+					<HeaderText>Web Development</HeaderText>
+					<Body>Learn the fundamentals of HTML, CSS, and JavaScript as you create your own website.</Body>
+    		</IconBox>
 			</Content>
 		</Page>
 	)
