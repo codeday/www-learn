240e652a15566e0da1dbd31f1170972055cce12a
Bobcat <bobcat4848@gmail.com>
2021-01-02 10:28:44 -0600

    Tracks load in from contentful now

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,10 @@
 import { Heading } from '@codeday/topo/Atom/Text';
 import Content from '@codeday/topo/Molecule/Content';
 import Divider from '@codeday/topo/Atom/Divider';
+import { Flex, Icon } from "@chakra-ui/core"
 import Text, { Link } from '@codeday/topo/Atom/Text';
+import List, { Item } from '@codeday/topo/Atom/List';
+import { apiFetch } from '@codeday/topo/utils';
 import Box from '@codeday/topo/Atom/Box';
 import Button from '@codeday/topo/Atom/Button';
 import Page from '../components/page';
@@ -13,6 +16,7 @@ const boxWidth = 330;
 const boxTextAlignment = "left";
 
 export default function Home({ tracks }) {
+	console.log(tracks);
 	return (
 		<Page slug="/">
 			<Content textAlign="center">
@@ -37,7 +41,7 @@ export default function Home({ tracks }) {
 				<Box>
 					<List>
 						<Flex size="100%" justify="left" alignItems="left" flexDirection="row" flexWrap="wrap">
-							{!(levels) ? (
+							{!(tracks) ? (
 								<>
 									<Item><Skelly /></Item>
 									<Item><Skelly /></Item>
@@ -50,72 +54,6 @@ export default function Home({ tracks }) {
 						</Flex>
 					</List>
 			</Box>
-
-
-
-
-				<IconBox mb={2} mr={2} textAlign={boxTextAlignment} maxWidth={boxWidth} d="inline-block">
-					<HeaderIcon><Image src="https://placekitten.com/64/64" borderRadius="full" /></HeaderIcon>
-					<HeaderText>Construct</HeaderText>
-					<Body>Start here if you're a complete begineer and finish by making your own game!
-						<br></br>
-						<Button variant="solid" variantColor="brand" href="/construct">Start</Button>
-					</Body>
-    		</IconBox>
-
-				<IconBox textAlign={boxTextAlignment} maxWidth={boxWidth} d="inline-block">
-					<HeaderIcon d="inline-block"><Image src="https://placekitten.com/64/64" borderRadius="full" /></HeaderIcon>
-					<HeaderText d="inline-block">Discord Bot</HeaderText>
-					<Body>Create your very own discord bot! Don't worry, you can pick your programming language!
-						<br></br>
-						<Button variant="solid" variantColor="brand">Start</Button>
-					</Body>
-    		</IconBox>
-
-				<IconBox textAlign={boxTextAlignment} maxWidth={boxWidth} d="inline-block">
-					<HeaderIcon><Image src="https://placekitten.com/64/64" borderRadius="full"/></HeaderIcon>
-					<HeaderText>Unity</HeaderText>
-					<Body>Take yourself to the next step and learn the fundamentals of using the Unity Engine
-						<br></br>
-						<Button variant="solid" variantColor="brand">Start</Button>
-					</Body>
-    		</IconBox>
-
-				<IconBox textAlign={boxTextAlignment} maxWidth={boxWidth} d="inline-block">
-					<HeaderIcon><Image src="https://placekitten.com/64/64" borderRadius="full"/></HeaderIcon>
-					<HeaderText>Unreal</HeaderText>
-					<Body>Take yourself to the next step and learn the fundamentals of using the Unreal Engine
-						<br></br>
-						<Button variant="solid" variantColor="brand">Start</Button>
-					</Body>
-    		</IconBox>
-
-				<IconBox textAlign={boxTextAlignment} maxWidth={boxWidth} d="inline-block">
-					<HeaderIcon><Image src="https://placekitten.com/64/64" borderRadius="full"/></HeaderIcon>
-					<HeaderText>Python</HeaderText>
-					<Body>Learn to program with Python, which is a simple and powerful language for begineers
-						<br></br>
-						<Button variant="solid" variantColor="brand">Start</Button>
-					</Body>
-    		</IconBox>
-
-				<IconBox textAlign={boxTextAlignment} maxWidth={boxWidth} d="inline-block">
-					<HeaderIcon><Image src="https://placekitten.com/64/64" borderRadius="full"/></HeaderIcon>
-					<HeaderText>Java</HeaderText>
-					<Body>Learn to program with Java, which has been tried and tested to be one of the best languages out there!
-						<br></br>
-						<Button variant="solid" variantColor="brand">Start</Button>
-					</Body>
-    		</IconBox>
-
-				<IconBox textAlign={boxTextAlignment} maxWidth={boxWidth} d="inline-block">
-					<HeaderIcon><Image src="https://placekitten.com/64/64" borderRadius="full"/></HeaderIcon>
-					<HeaderText>Web Development</HeaderText>
-					<Body>Learn the fundamentals of HTML, CSS, and JavaScript as you create your own website.
-						<br></br>
-						<Button variant="solid" variantColor="brand">Start</Button>
-					</Body>
-    		</IconBox>
 			</Content>
 		</Page>
 	)
@@ -123,18 +61,14 @@ export default function Home({ tracks }) {
 
 function TrackBox({ info }) {
   return (
-  <Box marginRight={3} w="275px">
-        <Box borderWidth="2px" p={3} w="100%" borderColor={level.boxColor} roundedTopLeft="lg" roundedTopRight="lg" color={level.titleColor} backgroundColor={level.boxColor}>
-          {level.name}
-        </Box>
-        <Box borderWidth="2px" borderColor={level.borderColor} roundedBottomLeft="lg" roundedBottomRight="lg">
-          <Box backgroundColor={level.boxTint} p={3} w="100%">
-            <strong>${level.amount}/{level.amountInterval}</strong><br></br>
-            {level.description}
-          </Box>
-        </Box>
-
-  </Box>
+		<IconBox mb={2} mr={2} textAlign="left" maxWidth={325}>
+			<HeaderIcon h={75}><Image w="25%" src={info.logo.url} /></HeaderIcon>
+			<HeaderText>{info.name}</HeaderText>
+			<Body>{info.description}
+				<br></br>
+				<Button mt={2} variant="solid" variantColor="brand" href="/">Start</Button>
+			</Body>
+		</IconBox>
   );
 }
 
@@ -144,7 +78,9 @@ const query = () => `{
       items {
         name
 				description
-				logo
+				logo {
+					url
+				}
       }
     }
   }
@@ -154,7 +90,7 @@ export async function getStaticProps() {
 	const data = await apiFetch(query());
 	return {
 		props: {
-			tracks: data?.learn?.tracks || null,
+			tracks: data?.learn?.tracks?.items || null,
 		},
 		revalidate: 120,
 	}
