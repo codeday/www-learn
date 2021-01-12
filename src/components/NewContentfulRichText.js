c2f92326798fbd737f2cf32162e48c4ebe8a7600
Bobcat <bobcat4848@gmail.com>
2021-01-12 14:09:47 -0600

    Create NewContentfulRichText.js

---
+++ src/components/NewContentfulRichText.js
@@ -0,0 +1,36 @@
+import { BLOCKS } from '@contentful/rich-text-types';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+ 
+const document = {
+  nodeType: 'document',
+  content: [
+    {
+      nodeType: 'embedded-entry-block',
+      data: {
+        target: (...)Link<'Entry'>(...);
+      },
+    },
+  ]
+};
+ 
+const CustomComponent = ({ title, description }) => (
+  <div>
+    <h2>{title}</h2>
+    <p>{description}</p>
+  </div>
+);
+ 
+const options = {
+  renderNode: {
+    [BLOCKS.EMBEDDED_ENTRY]: (node) => {
+      const { title, description } = node.data.target.fields;
+      return <CustomComponent title={title} description={description} />
+    },
+    [INLINES.EMBEDDED_ENTRY]: (node) => {
+      console.log(node.data.target.fields);
+    }
+  }
+};
+ 
+documentToReactComponents(document, options);
+// -> <div><h2>[title]</h2><p>[description]</p></div>
