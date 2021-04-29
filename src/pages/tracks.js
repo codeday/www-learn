d7baedb90802a1de3e7ba8b9ab14cf5d2f4a2023
Bobcat <bobcat4848@gmail.com>
2021-04-29 17:24:05 -0500

    tracks page acting weird

--- src/pages/tracks.js
+++ src/pages/tracks.js
@@ -3,18 +3,18 @@ import { print } from 'graphql';
 import { apiFetch } from '@codeday/topo/utils';
 import Page from '../components/page';
 import BrowseTracks from '../components/BrowseTracks';
-import { BrowseTracksQuery } from '../components/BrowseTracks.gql'
+import { IndexQuery } from './index.gql';
 
-export default function Home({ tracks }) {
+export default function Tracks({ tracks }) {
   return (
-    <Page slug="/">
+    <Page slug="/tracks">
       <BrowseTracks tracks={tracks}/>
     </Page>
   );
 }
 
 export async function getStaticProps() {
-  const data = await apiFetch(print(BrowseTracksQuery));
+  const data = await apiFetch(print(IndexQuery));
   return {
     props: {
       tracks: data?.learn?.tracks?.items || {},
