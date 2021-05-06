27f433cb2c11f6437bd6b451c17ca76e4c150590
Bobcat <bobcat4848@gmail.com>
2021-05-06 13:38:55 -0500

    more work on account page

--- src/pages/account.js
+++ src/pages/account.js
@@ -17,12 +17,21 @@ export default function Account() {
       {session &&
         <Content>
           <Flex>
-            <Box>
+            <Box textAlign="center">
               <Image src={session.user.picture} borderRadius="full" />
+              <Text mb={0} fontSize={25}>Hello {session.user.given_name}!</Text>
+              <Text mt={0} fontSize={13} textDecoration="underline"><a href="https://account.codeday.org/">Edit account</a></Text>
             </Box>
   
-            <Box>
+            <Box textAlign="center" w="100%">
               <Heading>Statistics</Heading>
+              <Flex justifyContent="center" mt={5}>
+                <StatisticsBox statName="Obtained Points" statValue="0"></StatisticsBox>
+
+                <StatisticsBox statName="Completed Lessons" statValue="0"></StatisticsBox>
+
+                <StatisticsBox statName="Completed Tracks" statValue="0"></StatisticsBox>
+              </Flex>
             </Box>
           </Flex>
 
@@ -32,4 +41,13 @@ export default function Account() {
       }
     </Page>
   );
+}
+
+function StatisticsBox({ statName, statValue }) {
+  return (
+    <Box backgroundColor="#69B9FF" p={5} w={225} borderRadius={5} textAlign="left" ml={5}>
+      <Heading fontSize={20}>{statName}</Heading>
+      <Text mb={0}>{statValue}</Text>
+    </Box>
+  )
 }
